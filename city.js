c5d4119e7b57129d56b3a51f18a0b2e1cdd4c7b7
AKASH RAJ <akashraj9828@gmail.com>
2017-10-15 08:36:57 +0530

    re-creating

---
+++ city.js
@@ -0,0 +1,6 @@
+function city(x,y,num){
+
+  this.pos=createVector(x,y)
+  this.id=num
+
+}
--- sketch.js
+++ sketch.js
@@ -5,87 +5,88 @@ var tries=0;
 var possibilities=fact(totalCities);
 var time=0;
 var totalTime;
+var pause=false
 
-//console.log("variables");
+
 function setup() {
-//	console.log("setup function");
-createCanvas(400,800)
-// var x=(windowWidth - width) / 2;
-// var y=(windowHeight - height) / 2;
-// cnv.position(x, y);
-// //frameRate(1);
-for (var i=0;i<totalCities;i++){
-	v=createVector(random(width),random(height)/2);
-cities[i]=v;
+	var canvas=createCanvas(400,800)
+	// //frameRate(1);
+	for (var i=0;i<totalCities;i++){
+		x=random(width)
+		y=random(height/2)
+		cities[i]=new city(x,y,i);
+		
 
+		// var d=calcDistance(cities);
+		// recordDistance=d;
+		// bestRoute=cities.slice();
 
-	var d=calcDistance(cities);
-	recordDistance=d;
-	bestRoute=cities.slice();
-
-}
+	}
 
 }
 
 
 function draw() {
 
-  background(51);
-  fill(255);
-	stroke("red")
-  for (var i=0;i<cities.length;i++){
-	ellipse(cities[i].x,cities[i].y,4,4)
-  }
-  stroke(255);
-  strokeWeight(0.2);
-  noFill();
+	background(51);
+	fill(255);
+	
+	stroke(255);
+	strokeWeight(0.2);
+	noFill();
 	beginShape();
 	for (var i=0;i<cities.length;i++){
-	vertex(cities[i].x,cities[i].y)
+		push()
+		stroke("red")
+		ellipse(cities[i].pos.x,cities[i].pos.y,2,2)
+		pop()
+		text(cities[i].id,cities[i].pos.x,cities[i].pos.y)	
+		vertex(cities[i].pos.x,cities[i].pos.y)
 	}
 	endShape();
+	
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  fill(255);
-	stroke("yellow")
-	for (var i=0;i<bestRoute.length;i++){                             ///////////////////////////
-	ellipse(bestRoute[i].x,bestRoute[i].y+ (height/2),4,4)            //////////////////////////
-}
+	// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// fill(255);
+	// stroke("yellow")
+	// for (var i=0;i<bestRoute.length;i++){                             ///////////////////////////
+	// 	ellipse(bestRoute[i].x,bestRoute[i].y+ (height/2),4,4)            //////////////////////////
+	// }
 
 
-stroke("red");                                           //////////////////////////
-	strokeWeight(5);
-	noFill();
+	// stroke("red");                                           //////////////////////////
+	// strokeWeight(5);
+	// noFill();
 
-	  line(0, height/2, width, height/2);
-	                                                               /////////////////////////final down window
-	stroke("SpringGreen");                                           //////////////////////////
-    strokeWeight(1);
-    noFill();
-	beginShape();
-	for (var i=0;i<bestRoute.length;i++){
-	vertex(bestRoute[i].x,bestRoute[i].y+ (height/2))
-	}
-	endShape();
+	// line(0, height/2, width, height/2);
+	// /////////////////////////final down window
+	// stroke("SpringGreen");                                           //////////////////////////
+	// strokeWeight(1);
+	// noFill();
+	// beginShape();
+	// for (var i=0;i<bestRoute.length;i++){
+	// 	vertex(bestRoute[i].x,bestRoute[i].y+ (height/2))
+	// }
+	// endShape();
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+	// //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-	var i=floor(random(cities.length));
-	var j=floor(random(cities.length));
 
-	swap(cities,i,j);
 
-	var d=calcDistance(cities);
-	if(d<recordDistance){
-		recordDistance=d;
-		bestRoute=cities.slice();
-		console.log(recordDistance);
+	// var i=floor(random(cities.length));
+	// var j=floor(random(cities.length));
 
-	}
+	// swap(cities,i,j);
+
+	// var d=calcDistance(cities);
+	// if(d<recordDistance){
+	// 	recordDistance=d;
+	// 	bestRoute=cities.slice();
+	// 	console.log(recordDistance);
+
+	// }
 
 
 }
@@ -98,9 +99,7 @@ function timeElapsed(){
 
 }
 
-
-function swap(a,i,j)
-{
+function swap(a,i,j){
 	tries++;
 	//console.log(tries);
 	var percent=100*tries/possibilities;
@@ -116,14 +115,8 @@ function swap(a,i,j)
 	document.getElementById("demo3").innerHTML="best distance till now :"+recordDistance;
 
 }
+
 setInterval("timeElapsed()",1000);
-
-
-
-
-
-
-
 
 function calcDistance(points){
 	var sum=0;
@@ -137,18 +130,24 @@ function calcDistance(points){
 
 }
 
-
 function fact(num) {
-    var result = 1;
-    for (var i = 2; i <= num; i++) {
-        result *= i;
+	var result = 1;
+	for (var i = 2; i <= num; i++) {
+		result *= i;
 
-    }
-    return result;
+	}
+	return result;
 }
 
+function keyPressed() {
+	if(key=='p'||key=='P'){
+		if(pause){
+			pause=false
+			loop;
+		}else if(!pause){
+			pause=true;
+			noLoop();
+		}
+	}
 
-// var par=document.createElement("p");
-// var tex=document.createTextNode("hey how u doin");
-// par.appendChild(tex);
-// document.getElementById("head").appendChild(par);
+}
